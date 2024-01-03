05d9553bb5685c4dfef135073c20dff8da9964c5
Rick <rick@portto.com>
2024-01-03 16:22:26 +0800

    inscription domain & adapter

--- internal/adapter/repo_inscription.go
+++ internal/adapter/repo_inscription.go
@@ -1,1 +1,25 @@
 package adapter
+
+import (
+	"context"
+	"flow-indexer/internal/domain/inscription"
+
+	"gorm.io/gorm"
+)
+
+type inscriptionRepo struct {
+	db *gorm.DB
+}
+
+func NewInscriptionRepo(db *gorm.DB) inscription.Repository {
+	return &inscriptionRepo{db: db}
+}
+
+func (r *inscriptionRepo) GetByInscriptionAndAddress(ctx context.Context, insName, address string) (*inscription.Balance, error) {
+	var balance inscription.Balance
+	err := r.db.WithContext(ctx).Where("inscription = ? AND account = ?", insName, address).First(&balance).Error
+	if err != nil {
+		return nil, err
+	}
+	return &balance, nil
+}
